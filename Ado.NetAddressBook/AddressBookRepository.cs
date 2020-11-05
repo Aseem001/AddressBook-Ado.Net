b3fb793a5693b232aea62047c4ff9ce3b8d7e96c
Aseem <aseemanand2@gmail.com>
2020-11-06 00:23:38 +0530

    [Aseem] Added code to initialize the connection and get all contact details

---
+++ Ado.NetAddressBook/AddressBookRepository.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBookRepository.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Ado.NetAddressBook
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Text;
+
+    public class AddressBookRepository
+    {
+        public static SqlConnection connection { get; set; }
+
+        public void GetAllContacts()
+        {
+            //Creates a new connection for every method to avoid "ConnectionString property not initialized" exception
+            DBConnection dbc = new DBConnection();
+            connection = dbc.GetConnection();
+            AddressBookModel model = new AddressBookModel();
+            string query = @"select * from dbo.address_book";
+            try
+            {
+                using (connection)
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            model.FirstName = reader.GetString(0);
+                            model.LastName = reader.GetString(1);
+                            model.Address = reader.GetString(2);
+                            model.City = reader.GetString(3);
+                            model.State = reader.GetString(4);
+                            model.Zip = reader.GetInt32(5);
+                            model.PhoneNumber = reader.GetInt64(6);
+                            model.Email = reader.GetString(7);
+                            model.AddressBookName = reader.GetString(8);
+                            model.ContactType = reader.GetString(9);
+                            Console.WriteLine($"First Name: {model.FirstName}\nLast Name: {model.LastName}\nAddress: {model.Address}\nCity: {model.City}\nState: {model.State}\nZip: {model.Zip}\nPhone Number: {model.PhoneNumber}\nContact Type: {model.ContactType}\nAddress Book Name : {model.AddressBookName}");
+                            Console.WriteLine("\n");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (connection.State.Equals("Open"))
+                    connection.Close();
+            }
+        }
+    }
+}
---
+++ Ado.NetAddressBook/Program.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Aseem Anand"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Ado.NetAddressBook
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBookRepository addressBookRepo = new AddressBookRepository();
+            addressBookRepo.GetAllContacts();
+        }
+    }
+}
